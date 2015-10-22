bac2d6f26ce8c8b0155ab0134b0e4ea823e51b08
Saberprivateer <daniel.kamerling@gmail.com>
2015-10-22 15:57:52 -0700

    now with gulp

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+
+var concat = require('gulp-concat');
+
+gulp.task('scripts', function () {
+    return gulp.src([
+        'js/vendor/modernizr.js',
+        'bower_components/jquery/dist/jquery.js',
+        'bower_components/angular/angular.js',
+        'bower_components/angular-foundation/mm-foundation-tpls.js',
+        'bower_components/angular-route/angular-route.js',
+        'bower_components/angular-animate/angular-animate.min.js',
+        'bower_components/angular-aria/angular-aria.min.js',
+        'bower_components/angular-material/angular-material.min.js',
+        'js/foundation.min.js',
+        'js/foundation/foundation.reveal.js'])
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('./js/'));
+});
+
+gulp.task('default', ['scripts'], function () {
+    // place code for your default task here
+});
