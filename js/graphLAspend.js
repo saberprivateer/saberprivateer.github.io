2c8a772b65aaaaaec8c2eff85d79224104ca8a13
saberprivateer <daniel.kamerling@gmail.com>
2015-10-25 18:33:21 -0700

    graph werks...not dynamic

---
+++ js/graphLAspend.js
@@ -0,0 +1,76 @@
+var margin = {top: 30, right: 120, bottom: 0, left: 120},
+    width = 800,
+    height = 140;
+
+var x = d3.scale.linear()
+    .range([0, width-200]);
+
+var barHeight = 20;
+
+var color = d3.scale.ordinal()
+    .range(["steelblue", "#ccc"]);
+
+/*
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+svg.append("rect")
+    .attr("class", "background")
+    .attr("width", width)
+    .attr("height", height)
+//    .on("click", up);
+
+svg.append("g")
+    .attr("class", "x axis");
+
+svg.append("g")
+    .attr("class", "y axis")
+  .append("line")
+    .attr("y1", "100%");
+*/
+var chart = d3.select(".chart")
+.attr("width",width);
+
+d3.json("js/test.json", function(error, root) {
+  if (error) throw error;
+
+console.log(root[0]);
+
+root.forEach(function(d) {
+    d.dollar_amount = +d.dollar_amount;
+    });
+
+var newroot = d3.nest()
+.key(function(d){return d.government_activity})
+//.key(function(d){return d.expenditure_type})
+//.key(function(d){return d.department_name})
+.rollup(function(values) {return d3.sum(values, function(d){return d3.round(+d.dollar_amount);}) })
+.entries(root);
+
+console.log("This is after nest: ");
+console.log(newroot);
+
+  x.domain([0, d3.max(newroot, function(d) { return d.values; })]);
+
+  chart.attr("height", barHeight * newroot.length);
+
+var bar = chart.selectAll("g")
+      .data(newroot)
+    .enter().append("g")
+      .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+
+  bar.append("rect")
+      .attr("width", function(d) { return x(d.values); })
+      .attr("height", barHeight - 1);
+
+  bar.append("text")
+      .attr("x", function(d) { return x(d.values) + 5; })
+      //x(d.values) +
+      .attr("y", barHeight / 2)
+      .attr("dy", ".35em")
+      .text(function(d) { return  d.key+" "+d3.format("$,")(d.values); })
+
+});
--- js/laspendapp.js
+++ js/laspendapp.js
@@ -2,5 +2,19 @@
 
 var laspendApp = angular.module('laspendApp', [
     'ngRoute',
-    'ngMaterial'
+    'ngMaterial',
+
 ]);
+
+laspendApp.controller('laspendCtrl', function($scope, $http){
+//$http.get('https://controllerdata.lacity.org/resource/checkbook.json?calendar_month_year=SEPTEMBER,2015').success(function(data) {
+$http.get('js/test.json').success(function(data) {
+    console.log('success: ' + data.length);
+
+$scope.checkbook = data;
+    });
+
+    $scope.convertToInt= function (value) {
+                return parseInt(value,10);
+            };
+})
